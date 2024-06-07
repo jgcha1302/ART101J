e527542ebbfd01324ca7d6a200e608fe614afdd1
jgcha1302 <jgcha@ucsc.edu>
2024-06-07 00:43:25 -0700

    12:43

---
+++ lab9/js/lab.js
@@ -0,0 +1,20 @@
+/*
+  * Author: Joyce Cha <jgcha@ucsc.edu>
+  *Created: 26 May 
+  * License: Public Domain
+  */
+
+// Define Variables
+make= "New Ford";
+model="D40LF";
+color= "white";
+year= 1998;
+
+//calcuate
+age= 2024-year;
+
+//output
+document.writeLn("Make:" = make + "<br>");
+document.writeLn("Model:" + model + "<br>");
+document.writeLn("Color:" + color + "<br>");
+document.writeLn("Age:" + age + "years<br>");
