8a8d1be6a3d63fb74caa046a93ea93a38723afa3
jgcha1302 <jgcha@ucsc.edu>
2024-06-07 06:13:46 -0700

    613

---
+++ lab9/js/lab6.js
@@ -0,0 +1,24 @@
+/*
+* Author: Joyce Cha <jgcha@ucsc.edu>
+* Created: 26 May 
+*License: Public Domain
+*/
+myTransport = ["New Flyer", "metro", "rides from friends", "walk"]
+
+// Define Variables
+myMainRide = {
+    make: "New Flyer",
+    model: "D40LF",
+    color: "white",
+    year: 1998,
+    //defen a function and value of the year within your object 
+    age: function() {
+        return 2024 - this.year;
+    }
+}
+
+// output
+document.writeln("Kind of transportation I use:", myTransport, "</br");
+//write an object
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
