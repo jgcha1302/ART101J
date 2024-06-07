e0c4abc77d45dc0030420f0cf578f4e1b978d184
jgcha1302 <jgcha@ucsc.edu>
2024-06-06 23:05:19 -0700

    11:05

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -1,20 +1,21 @@
-//
-// Author: Joyce Cha <jgcha@ucsc.edu>
-// Created: 26 May 
-// License: Public Domain
-
+/*
+* Author: Joyce Cha <jgcha@ucsc.edu>
+* Created: 26 May 
+*License: Public Domain
+*/
 
 // Define Variables
-make= "New Flyer";
-model="D40LF";
-color= "white";
-year= 1998;
+make = "New Flyer";
+model ="D40LF";
+color = "white";
+year = 1998;
 
 //calcuate
-age= 2024-year;
+age = 2024 - year;
 
 // output
-document.writeLn("Make:" = make + "<br>");
+document.writeLn("Make:" + make + "<br>");
 document.writeLn("Model:" + model + "<br>");
 document.writeLn("Color:" + color + "<br>");
+document.writeLn("Year:" + year + "<br>");
 document.writeLn("Age:" + age + "years<br>");
