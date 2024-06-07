0907393c06af5a17bd3c19c3f2703bcc565163a3
jgcha1302 <jgcha@ucsc.edu>
2024-06-07 07:46:50 -0700

    746

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,23 +4,22 @@
 *License: Public Domain
 */
 // add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
 
-// add a click listener to the challenge button
-$("button-challenge").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
-    console.log("clicked challenge");
+  // click listener for button
+$("#make-convo").click(function(){
+    // get new fake dialogue
+    const newText = generateRandomText();
+    // append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
 });
 
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-
-$("#button-problems").click(function(){
-    $('#problems').toggleClass("special");
-    console.log("clicked problems");
-})
-
-$("#button-results").click(function(){
-    $("#results").toggleClass("special");
-    console.log("clicked results")
-});
