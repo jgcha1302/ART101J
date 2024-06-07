f19961b20e652b496136d7159086cc65b81b8644
jgcha1302 <jgcha@ucsc.edu>
2024-06-06 21:51:32 -0700

    9:51

---
+++ lab4 copy/js/lab.js
@@ -0,0 +1,32 @@
+// index.js - Tic-Tac-Toe 
+// Author: Joyce Cha
+// Date: 4/24/2024
+// An implementation of Tic Tac Towe
+// Joyce Cha <jgcha@ucsc.edu>
+// April 2024
+
+// Print the board before each turn
+    // Loop through each row
+        // Loop through each space
+            // Print X, O, or Space
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
+        
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,11 +1,11 @@
-//
-// Author: Joyce Cha <jgcha@ucsc.edu>
-// Created: 26 May 
-// License: Public Domain
-
+/*
+  * Author: Joyce Cha <jgcha@ucsc.edu>
+  *Created: 26 May 
+  * License: Public Domain
+  */
 
 // Define Variables
-make= "New Flyer";
+make= "New Ford";
 model="D40LF";
 color= "white";
 year= 1998;
@@ -13,7 +13,7 @@ year= 1998;
 //calcuate
 age= 2024-year;
 
-// output
+//output
 document.writeLn("Make:" = make + "<br>");
 document.writeLn("Model:" + model + "<br>");
 document.writeLn("Color:" + color + "<br>");
