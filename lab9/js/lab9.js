ff3a6eb55429ab31deaca7da3b8ccbaa11cce90b
jgcha1302 <jgcha@ucsc.edu>
2024-06-07 07:05:36 -0700

    705

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -10,10 +10,17 @@ $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 $("button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
+    console.log("clicked challenge");
 });
 
 $("#problems").append("<button id='button-problems'>Make Special</button>");
 
 $("#button-problems").click(function(){
-    $('#problems')
+    $('#problems').toggleClass("special");
+    console.log("clicked problems");
 })
+
+$("#button-results").click(function(){
+    $("#results").toggleClass("special");
+    console.log("clicked results")
+});
