662834405352ec651e73206a927593920426f070
jgcha1302 <jgcha@ucsc.edu>
2024-06-07 04:35:01 -0700

    4:34

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -3,22 +3,23 @@
 * Created: 26 May 
 *License: Public Domain
 */
-myTransport = ["New Flyer", "metro", "rides from friends", "walk"]
-
-// Define Variables
-myMainRide = {
-    make: "New Flyer",
-    model: "D40LF",
-    color: "white",
-    year: 1998,
-    //defen a function and value of the year within your object 
-    age: function() {
-        return 2024 - this.year;
-    }
+// sortUserName - a function that takes user input and sorts the letters
+function sortUserName() {
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.")
+    console.log("userName =", userName);
+    //split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray");
+    //sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArray =", nameArraySort);
+    //join array
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    return nameSorted;
 }
 
-// output
-document.writeln("Kind of transportation I use:", myTransport, "</br");
-//write an object
-document.writeln("My Main Ride: <pre>",
-    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+//output 
+document.written("Oh hey, I've fixed your name:",
+    sortUsername(), "</br>");
+    
